e6be97d5381aa54167ad3677f24841a18d176780
thongpb.hust <thongpb.hust@gmail.com>
2018-08-14 02:01:11 +0700

    add tuning accelerator

--- src/inference.py
+++ src/inference.py
@@ -6,21 +6,59 @@ import argparse
 import sys
 
 def parse_agrs(argv):
-	parser = argparse.ArgumentParser()
-	parser.add_argument('--model', type=str, help='path to trained model')
-	parser.add_argument('--data', type=str, help='path to test dataset')
-	return parser.parse_args(argv)
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--model', type=str, help='path to trained model')
+        parser.add_argument('--data', type=str, help='path to test dataset')
+        parser.add_argument('--batch_size', type=int, help="size of image batch")
+        parser.add_argument('--out', type=str, help='output file name')
+        return parser.parse_args(argv)
 
 
 
 def main(args):
         model = baseModel.Xception_Model(input_shape=(299,299,3),  batch_size = 128,
-                     num_classes = 103, trainable=False)
+                        num_classes = 103, trainable=False)
         model.sumary()
         model.load_model(args.model)
-        imgs = np.squeeze(util.process_image(args.data))
-        top1, top2, top3 = util.get_top_k(model, np.expand_dims(imgs, 0))
-        print (top1, top2, top3)
+
+        res_file = open(args.out, 'w')
+        res_file.write('id,predicted\n')
+
+        img_files, f_ids = util.list_imgs(args.data)
+        nb_sample = len(f_ids)
+        print("Number of test image: ", nb_sample)
+        nb_steps = int(np.ceil(len(f_ids)/args.batch_size))
+        error_ids = []
+        for step in range(nb_steps):
+                t_imgs, t_ids = [], []
+                print("Process with batch ", step)
+                for idx in range(args.batch_size):
+                        
+                        if((step*args.batch_size + idx) > nb_sample-1):
+                                break;
+                        # print("image: ", f_ids[step*args.batch_size + idx])
+                        try:
+                                t_img = util.process_image(img_files[step*args.batch_size + idx])
+                                # if (np.asarray(t_img).shape == (299,299,3)):
+                                t_imgs.append(t_img)
+                                t_ids.append(f_ids[step*args.batch_size + idx])
+                        except:
+                                print("Error: ", f_ids[step*args.batch_size + idx])
+                                error_ids.append(f_ids[step*args.batch_size + idx])
+                                continue
+                                # res_file.write(f_ids[step*args.batch_size + idx]+'\n')
+                predicted_res = util.get_top_k_(model, np.squeeze(t_imgs))
+
+                for j, idn in enumerate(t_ids):
+                        res_file.write(str(idn) + ', ' + str(predicted_res[j][-1]) + ' ' + str(predicted_res[j][-2]) + ' ' + str(predicted_res[j][-3]) + '\n')
+
+        for err in error_ids:
+                res_file.write(str(err) + ', ' + str(0) + ' ' + str(0) + ' ' + str(0) + '\n')
+        res_file.close()
+        print("Done....")
+        # imgs = np.squeeze(util.process_image(args.data))
+        # top1, top2, top3 = util.get_top_k(model, np.expand_dims(imgs, 0))
+        # print (top1, top2, top3)
 
 
 
--- src/utilities.py
+++ src/utilities.py
@@ -2,6 +2,8 @@ import numpy as np
 import os
 from skimage.io import imread
 from skimage.transform import resize
+import cv2
+import imageio
 
 def list_imgs(path):
     # images = [os.path.join(path, img) for img in os.listdir(dir) if (img[-4:] == '.png' or img[-4:] == '.jpg')]
@@ -14,13 +16,31 @@ def list_imgs(path):
     return images, img_id
 
 def process_image(img_path, img_size = (299,299)):
-    img = resize(imread(img_path), img_size)
+    img = None
+    try:
+        img = resize(cv2.cvtColor(cv2.imread(img_path), cv2.COLOR_BGR2RGB), img_size)
+    except:
+        print("read gif file ", img_path)
+        imgs = imageio.mimread(img_path)
+        if (len(np.asarray(imgs[0]).shape) >= 3):
+            # print(len(np.asarray(imgs[0]).shape))
+            # print(np.asarray(imgs[0]).shape)
+            img = cv2.resize(cv2.cvtColor(imgs[0], cv2.COLOR_RGBA2RGB), img_size)
+        else:
+            img = resize(cv2.cvtColor(imgs[0], cv2.COLOR_GRAY2RGB), img_size)
     img = np.asarray(img, dtype=np.float32)
-    img = img*(1./255)
-    return [img]
+    # img = img*(1./255)
+    return img
 
 def get_top_k(model, imgs, k=3):
     results = model.predict(imgs)
     predicted_labels = np.argsort(results, axis=1)
     top1, top2, top3 = np.squeeze(predicted_labels[:, -k:])
     return top1, top2, top3
+
+def get_top_k_(model, imgs, k=3):
+    results = model.predict(imgs)
+    # print("Prob", results)
+    predicted_labels = np.argsort(results, axis=1)
+    # print(predicted_labels)
+    return np.squeeze(predicted_labels[:, -k:])
